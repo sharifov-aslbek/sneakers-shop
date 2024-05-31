1272f7e52123c6d0ee507de31df7569b29027077
Sharifov Aslbek <148238968+sharifov-aslbek@users.noreply.github.com>
2024-05-31 17:56:25 +0500

    Fixed errors on Products 31/05

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <TheSlider />
   </section>
 
-  <section class="hero mt-10">
+  <section class="hero mt-10 mb-10">
     <div class="container">
       <div class="top flex items-center justify-between">
         <h2 class="title font-bold text-3xl">
@@ -15,23 +15,25 @@
         </form>
       </div>
       <div class="boxs flex items-center flex-wrap gap-10 mt-10">
-        <div class="box border w-[260px] h-[330px] py-3 px-4 rounded-3xl" v-for="item in getFilter">
+        <div class="box border w-[260px] h-[340px] py-3 px-4 rounded-3xl" v-for="item in getFilter">
           <div class="img-action">
             <img src="../assets/images/heart.svg" alt="#">
             <img :src="item.img" alt="">
           </div>
           <h2 class="title">{{ item.name }}</h2>
-          <div class="price flex items-center">
+          <div class="price flex items-center justify-between gap-5 mt-4">
             <div class="text">
-              <span>Цена:</span>
+              <span class="font-bold">Цена:</span>
               <h3>{{ item.price }} руб.</h3>
             </div>
 
-            <div class="add">
-              <img src="../assets/images/plus.svg" alt="">
+            <div class="add cursor-pointer">
+              <img :class="{'hidden' : item.id == this.activeAdded}" @click="getId(item.id); getProduct" src="../assets/images/plus.svg" alt="#">
+              <img v-show="item.id == this.activeAdded" src="../assets/images/checked.svg" alt="#">
             </div>
           </div>
         </div>
+        {{ getProduct }}
       </div>
     </div>
   </section>
@@ -48,7 +50,14 @@ export default {
     return {
       inputValue: '',
       array: productsArray,
+      isAdded: false,
+      activeAdded: 0
     }
+  },
+  methods: {
+    getId(id) {
+      this.activeAdded = id
+    },  
   },
   computed: {
     getFilter() {
@@ -57,11 +66,16 @@ export default {
       }
 
       return this.array.filter(item => item.name.indexOf(this.inputValue) > -1)
+    },
+    getProduct(){
+      return this.array.filter(item => item.id == this.activeAdded)
     }
   }
 }
 </script>
 
 <style>
-
+  .hidden {
+    display: none;
+  }
 </style>
