5eaa761616ccf784cf98eb4d6118ad61fb8b80c9
Sharifov Aslbek <148238968+sharifov-aslbek@users.noreply.github.com>
2024-05-31 16:19:35 +0500

    Created products on iterating array

---
+++ src/productsArray.js
@@ -0,0 +1,77 @@
+const array = [
+   {
+      id: 1,
+      name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+      img: 'src/assets/images/sneakers/sneakers-1.jpg',
+      price: '12999'
+   },
+   {
+      id: 2,
+      name: 'Мужские Кроссовки Nike Air Max 270',
+      img: 'src/assets/images/sneakers/sneakers-2.jpg',
+      price: '12999'
+   },
+   {
+      id: 3,
+      name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+      img: 'src/assets/images/sneakers/sneakers-3.jpg',
+      price: '8499'
+   },
+   {
+      id: 4,
+      name: 'Кроссовки Puma X Aka Boku Future Rider',
+      img: 'src/assets/images/sneakers/sneakers-4.jpg',
+      price: '8999'
+   },
+   {
+      id: 5,
+      name: 'Мужские Кроссовки Under Armour Curry 8',
+      img: 'src/assets/images/sneakers/sneakers-5.jpg',
+      price: '15199'
+   },
+   {
+      id: 6,
+      name: 'Мужские Кроссовки Nike Kyrie 7',
+      img: 'src/assets/images/sneakers/sneakers-6.jpg',
+      price: '11299'
+   },
+   {
+      id: 7,
+      name: 'Мужские Кроссовки Jordan Air Jordan 11',
+      img: 'src/assets/images/sneakers/sneakers-7.jpg',
+      price: '10799'
+   },
+   {
+      id: 8,
+      name: 'Мужские Кроссовки Nike LeBron XVIII',
+      img: 'src/assets/images/sneakers/sneakers-8.jpg',
+      price: '16499'
+   },
+   {
+      id: 9,
+      name: 'Мужские Кроссовки Nike Lebron XVIII Low',
+      img: 'src/assets/images/sneakers/sneakers-9.jpg',
+      price: '13999'
+   },
+   {
+      id: 10,
+      name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+      img: 'src/assets/images/sneakers/sneakers-10.jpg',
+      price: '8499'
+   },
+   {
+      id: 11,
+      name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+      img: 'src/assets/images/sneakers/sneakers-6.jpg',
+      price: '8499'
+   },
+   {
+      id: 12,
+      name: 'Мужские Кроссовки Nike Kyrie Flytrap IV',
+      img: 'src/assets/images/sneakers/sneakers-2.jpg',
+      price: '8499'
+   },
+   
+]
+
+export default array
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,66 @@
   <section class="slider">
     <TheSlider />
   </section>
+
+  <section class="hero mt-10">
+    <div class="container">
+      <div class="top flex items-center justify-between">
+        <h2 class="title font-bold text-3xl">
+          Все кроссовки
+        </h2>
+        <form class="flex gap-3 border py-3 px-3 rounded-xl">
+          <img src="../assets/images/search.svg" alt="#">
+          <input type="text" placeholder="Поиск..." v-model="inputValue" class="outline-none border-none">
+        </form>
+      </div>
+      <div class="boxs flex items-center flex-wrap gap-10 mt-10">
+        <div class="box border w-[260px] h-[330px] py-3 px-4 rounded-3xl" v-for="item in getFilter">
+          <div class="img-action">
+            <img src="../assets/images/heart.svg" alt="#">
+            <img :src="item.img" alt="">
+          </div>
+          <h2 class="title">{{ item.name }}</h2>
+          <div class="price flex items-center">
+            <div class="text">
+              <span>Цена:</span>
+              <h3>{{ item.price }} руб.</h3>
+            </div>
+
+            <div class="add">
+              <img src="../assets/images/plus.svg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import TheSlider from '../components/TheSlider.vue'
+import productsArray from '../productsArray.js'
 export default {
   components: {
     TheSlider
+  },
+  data() {
+    return {
+      inputValue: '',
+      array: productsArray,
+    }
+  },
+  computed: {
+    getFilter() {
+      if(this.inputValue.length == 0) {
+        return this.array
+      }
+
+      return this.array.filter(item => item.name.indexOf(this.inputValue) > -1)
+    }
   }
 }
 </script>
 
-<style></style>
+<style>
+
+</style>
